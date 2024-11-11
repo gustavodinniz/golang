487d458ef53f4459ad8c2875967d6e71d8a41412
Gustavo Diniz <gustavodinniz.dev@gmail.com>
2024-11-11 11:46:08 -0300

    feat: configuring git and creating initial course structure

---
+++ cod3r/fundamentals/first/first.go
@@ -0,0 +1,13 @@
+// Executable programs start with the main package
+package main
+
+/*
+Code in Go are organized into packages and to use them you need to declare
+one or more imports.
+*/
+import "fmt"
+
+// The main function is the entry point of the program
+func main() {
+	fmt.Println("Hello World")
+}
